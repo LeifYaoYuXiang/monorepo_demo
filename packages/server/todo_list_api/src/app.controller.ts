b55fbf8a8ede0164efc437abc42dc2306cc0f550
Yuxiang Yao <yaoyuxiangyyx2009@126.com>
2022-09-22 19:31:13 +0800

    modify the backend server

--- packages/server/todo_list_api/src/app.controller.ts
+++ packages/server/todo_list_api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('demo_test')
+  getDemoTest(): string {
+    return 'demo test';
+  }
+
+  @Post('post_data')
+  postData(@Body() new_todo): string {
+    console.log(new_todo['param1']);
+    return 'HELLO WORLD';
   }
 }
--- packages/server/todo_list_api/src/app.service.ts
+++ packages/server/todo_list_api/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  saveData(todo_str) {
+    return 'HelloWorld';
+  }
 }
